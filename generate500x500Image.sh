a511bc8acc39ab678bf9dcf3c39c2a115432feb3
TBSten <TBSten@users.noreply.github.com>
2025-11-25 16:03:58 +0000

    deploy: 06f26c88b6bafd028896049dbe8629298d693b87

---
+++ generate500x500Image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p 500x500
+for img in base/*; do
+  case "${img##*.}" in
+    png|jpg|jpeg) ;;
+    *) continue ;;
+  esac
+  [ -e "$img" ] || continue
+  filename=$(basename "$img")
+  echo "start transform: $filename"
+  convert "$img" -resize 500x500^ -gravity center -extent 500x500 "500x500/$filename"
+  echo "finish transform: $filename"
+done
+
